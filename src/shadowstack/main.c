8ae2337ebaf65de12fdbe321db314a212c73e628
davide-moletta <davide.moletta@studenti.unitn.it>
2024-06-11 15:10:56 +0200

    added fix for puts

--- src/shadowstack/main.c
+++ src/shadowstack/main.c
@@ -1,23 +1,10 @@
 #include <mdk.h>
 
-// int main(void) {
-//   wdt_disable();
-//   int a = 2 +2;
-//   printf("a: %d", a);
-
-//   return 0;
-// }
-
 //__attribute__((section(".prova"))) void exception_handler(){
-__attribute__((section(".prova"))) void exception_handler(){
+void exception_handler(){
 
     printf("Exception");
-    while (1)
-    {
-        /* code */
-    }
-    
-
+    while (1);
     asm("nop");
 }
 
@@ -26,60 +13,62 @@ void trap_base() {
     asm("j exception_handler");
 }
 
-void user_mode_entry_point(){ //0x42007d38
-    asm("la t0, 0x0"); 
-    asm("csrw mie, t0"); 
-    printf("Hello from user mode");
+void user_mode_entry_point(){
+    // asm("la t0, 0x0"); 
+    // asm("csrw mie, t0"); 
+    printf("Hello from user mode!\n");
     while(1);
     asm("nop");
-   // return 0;
 }
-
-/*MSTATUS REGISTER
-
-31 30-23 22  21 20  19  18  17   16-15   14-13   12-11    10-9    8   7    6   5    4    3   2    1   0
-SD WPRI  TSR TW TVM MXR SUM MPRV XS[1:0] FS[1:0] MPP[1:0] VS[1:0] SPP MPIE UBE SPIE WPRI MIE WPRI SIE WPRI
-
-*/
 
 int main(void)
 {
-    wdt_disable();
+    wdt_disable(); // Disable watchdog to avoid continuous resets
 
     asm("la t0, trap_base");
     asm("csrw mtvec, t0");
-    /*
-        Load mstatus in t0
-        Load user mode status in t1
-        And to change MPP bits in mstatus to user mode
-        Or to change MIE bits in mstatus to 0
-        Write new mstatus
-    */
-    asm("csrr t0, mstatus"); 
-    asm("li t1, 0xFFFFE7FF"); //    11111111111111111110011111111111
-    asm("and t0, t0, t1");
-    asm("or t0, t0, 8"); //    0000000000000000000000000001000
-    asm("csrw mstatus, t0");
-    printf("Hello world from the ESP board!");
 
     /*
-        Load user mode address entry point
-        Write mepc (machine pc) to the user entry point (used by mret)
+        MSTATUS REGISTER (bits: [position])
+
+        SD:     [31]                    VS:     [10:9]
+        WPRI:   [30:23]                 SPP:    [8]
+        TSR:    [22]                    MPIE:   [7]
+        TW:     [21]                    UBE:    [6]
+        TVM:    [20]                    SPIE:   [5]
+        MXR:    [19]                    WPRI:   [4]
+        SUM:    [18]                    MIE:    [3]
+        MPRV    [17]                    WPRI:   [2]
+        XS:     [16:15]                 SIE:    [1]
+        FS:     [14:13]                 WPRI:   [0]
+        MPP:    [12:11]
     */
-    asm("la t0, user_mode_entry_point");
-    asm("csrw mepc, t0"); 
+
+    asm("csrr t0, mstatus");    // Load mstatus in t0
+    asm("li t1, 0xFFFFE7FF");   // Load user mode status in t1
+    asm("and t0, t0, t1");      // And to change MPP bits in mstatus to user mode
+    asm("or t0, t0, 8");        // Or to change MIE bits in mstatus to 0
+    asm("csrw mstatus, t0");    // Write new mstatus
 
 
+    printf("Hello world from the ESP board!\n");
+
+
+    asm("la t0, user_mode_entry_point");    // Load user mode address entry point
+    asm("csrw mepc, t0");                   // Write mepc (machine pc) to the user entry point (used by mret)
+
+    // PMP configuration
     asm("li t0, 0x90000000");
     asm("srli t0, t0, 2");
     asm("csrw pmpaddr0, t0");
     asm("li t0, 0x0707070F");
     asm("csrw pmpcfg0, t0");
 
-    asm("mret");
-    printf("Hello world from the ESP board!");
+    asm("mret"); // Jump to user code in user mode
+
+    printf("Hello world from the ESP board!\n");
+
     while (1);
-    
     return 0;
 }
 
