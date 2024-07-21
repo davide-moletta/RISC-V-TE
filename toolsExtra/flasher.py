d501f25ce107102a060c6d95fd1b37a7c6111346
davide-moletta <davide.moletta@studenti.unitn.it>
2024-07-21 18:16:11 +0200

    added compilation from independent assembly files, working on instrumentation

--- toolsExtra/flasher.py
+++ toolsExtra/flasher.py
@@ -30,7 +30,7 @@ OUTPUT = sys.argv[1] # Name of the output file
 def run_command(command, new_file):
     if(new_file):
         command_list = shlex.split(command)
-        with open(f"{DIRECTORY}/toolsExtra/{OUTPUT}.S", "w") as output_file:
+        with open(f"{DIRECTORY}/toolsExtra/{OUTPUT}.s", "w") as output_file:
             with open(f"{DIRECTORY}/toolsExtra/warnings.log", "a") as error_file:
                 subprocess.run(command_list, stdout=output_file, stderr=error_file, check=True)
     else:
@@ -44,11 +44,10 @@ def run_command(command, new_file):
 # Removes old .elf, .bin and .S files before building new ones
 def clear():
     print("Clearing previous files...")
-    files_to_delete = " ".join([f"{DIRECTORY}/toolsExtra/{file}" for file in os.listdir(DIRECTORY + "/toolsExtra") if file.endswith(('.bin', '.elf', '.S', ".log"))])
+    files_to_delete = " ".join([f"{DIRECTORY}/toolsExtra/{file}" for file in os.listdir(DIRECTORY + "/toolsExtra") if file.endswith(('.bin', '.elf', '.s', ".log"))])
     if (files_to_delete != ""):
         remove_command = f"rm {files_to_delete}"
-        command_list = shlex.split(remove_command)
-        subprocess.run(command_list, check=True)
+        run_command(remove_command, False)
         print("Files deleted successfully")
     else:
         print("No files to delete")
@@ -85,11 +84,50 @@ def run():
     run_command(monitor_command, False)
 
 def secure_build():
-    print("Securely building the sources...")
+    clear() # Remove files
+    print("Retrieving user source files...")
+
+    # Retrieve user files in the sub folder
+    extra_sources = " ".join([f"{SOURCES_DIRECTORY}/usercode/{file}" for file in os.listdir(SOURCES_DIRECTORY + "/usercode") if file.endswith(".c")])
+    files_to_instrument = extra_sources.replace(".c", ".h").replace("src/shadowstack/usercode/", "toolsExtra/")
+
+    print("Creating individual assembly files...")
+    assembly_command = f"{TOOLCHAIN}-gcc -S {CFLAGS} {SOURCES} {extra_sources}" # Builds the individual assembly files with toolchain
+    run_command(assembly_command, False)
+
+    # INSTRUMENT CODE HERE
+
+    all_assembly_files = " ".join([f"{DIRECTORY}/toolsExtra/{file}" for file in os.listdir(DIRECTORY + "/toolsExtra") if file.endswith(".s")])
+
+    print("Creating the .elf file...")
+    elf_command = f"{TOOLCHAIN}-gcc {all_assembly_files} {LINKFLAGS} -o {OUTPUT}.elf" # Builds the .elf file with toolchain
+    run_command(elf_command, False)
+
+    print("Creating the .bin file...")
+    bin_command = f"{ESPUTIL} mkbin {OUTPUT}.elf {OUTPUT}.bin" # Creates the .bin file from the .elf file with Espressif utils
+    run_command(bin_command, False)
+
+    print("Creating the .S file...")
+    dump_command = f"{TOOLCHAIN}-objdump -D {OUTPUT}.elf" # Creates the .S file (for inspections)
+    run_command(dump_command, True)
+
+    print("Clearing assembly files...")
+    remove_command = f"rm {all_assembly_files}" # Removes assembly files after using them to build the .elf file
+    run_command(remove_command, False)
+   
+    print("Files built successfully")
 
 def secure_run():
     secure_build()
-    print("Securely running the executable...")
+    
+    print("Flashing the instrumented program...")
+
+    flash_command = f"{ESPUTIL} flash {FLASH_ADDR} {OUTPUT}.bin" # Flashes the .bin program to the board
+    run_command(flash_command, False)
+
+    monitor_command = f"{ESPUTIL} monitor" # Monitor used to see the execution on the board
+    run_command(monitor_command, False)
+
 
 match sys.argv[2]:
     case "build":
